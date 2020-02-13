bda49fd8915b09ccff9189c546e1c9b9353c8f16
gregor61295 <gregor61295@GB-LD1LPW10G004.mastek.com>
2020-02-13 15:31:26 +0000

    Employee API

---
+++ hrapp/src/main/java/com/mastek/hrapp/apis/APIConfig.java
@@ -0,0 +1,15 @@
+package com.mastek.hrapp.apis;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import com.mastek.hrapp.services.EmployeeService;
+
+@Component //declare it as Spring Component
+public class APIConfig extends ResourceConfig{ // configure the service classes using ResourceConfig
+	
+	public APIConfig() { //register each service class to enable services as API
+		register (EmployeeService.class);
+	}
+
+}
